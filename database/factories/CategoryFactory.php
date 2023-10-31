504c2328a693e8487fe74f5f1586963c5be168cf
Matheus Quessada <matheusquessada@hotmail.com>
2023-10-30 20:59:46 -0700

    Models factories created

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->words(4, true),
+            'description' => fake()->text('100'),
+            'user_id' => User::factory()
         ];
     }
 }
--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,13 @@ class VideoFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'description' => fake()->text('200'),
+            'privacy' => fake()->randomElement(['listed', 'unlisted', 'private']),
+            'thumbnail' => 'empty-thumbnail.jpg',
+            'file_reference' => 'www.no-url-video.com',
+            'user_id' => User::factory(),
+            'category_id' => Category::factory()
         ];
     }
 }
